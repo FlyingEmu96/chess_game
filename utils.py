51b3925ce68b1af3680c60cf2f56768a88b40899
Emir_Flying_Emu96 <emir.murat1337@gmail.com>
2025-09-03 14:46:57 +0100

    Add utility functions and Color class for chess

---
+++ utils.py
@@ -0,0 +1,18 @@
+# utils.py
+FILES = "abcdefgh"
+RANKS = "12345678"
+
+def sq_to_xy(sq: str):
+    f, r = sq[0], sq[1]
+    return FILES.index(f), RANKS.index(r)
+
+def xy_to_sq(x: int, y: int) -> str:
+    return f"{FILES[x]}{RANKS[y]}"
+
+class Color:
+    WHITE = 0
+    BLACK = 1
+
+    @staticmethod
+    def other(c: int) -> int:
+        return Color.BLACK if c == Color.WHITE else Color.WHITE
